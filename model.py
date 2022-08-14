c3ce3076b9cc9ae495466ebe8500ca17d05323f1
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-08-14 20:35:07 -0300

    starting model

---
+++ model.py
@@ -0,0 +1,12 @@
+class Movie:
+    def __init__(self, name, year, length):
+        self.name = name
+        self.year = year
+        self.length = length
+
+class Tv_Shows:
+    
+
+
+avengers = Movie("Avengers - Infinity War", 2018, 160)
+print(avengers.name)
