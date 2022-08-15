e3cad13877d3ea0f5f7cc3537b525c637862b745
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-08-14 23:18:02 -0300

    Update model.py

--- model.py
+++ model.py
@@ -1,12 +1,23 @@
+from codecs import namereplace_errors
+
+
 class Movie:
     def __init__(self, name, year, length):
         self.name = name
         self.year = year
         self.length = length
 
-class Tv_Shows:
+class Series:
+    def __init__(self, name, year, seasons):
+        self.name = name
+        self.year = year
+        self.seasons = seasons 
     
 
 
 avengers = Movie("Avengers - Infinity War", 2018, 160)
-print(avengers.name)
+print(f'Name: {avengers.name} - Year: {avengers.year} - Length (minutes): {avengers.length}')
+
+atlanta = Series('Atlanta', 2018, 2)
+
+print(f'Name: {atlanta.name} - Year: {atlanta.year} - Seasons: {atlanta.seasons}')
